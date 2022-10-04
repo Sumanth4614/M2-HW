d8cf5249c242ab6100dd4cb914530060b51eab3f
Sumanth4614 <114673746+Sumanth4614@users.noreply.github.com>
2022-10-04 15:16:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let FirstName = "Sumanth";
+let LastName = "Tunga";
+let Space = " ";
+let hello = "Hello";
+let myName = "My name is";
+let FullName = "Sumanth Tunga";
+console.log(FirstName + Space + LastName);
+console.log("hello, My name is:", FirstName + Space + LastName);
+console.log(hello + Space + myName + Space + FullName);
+let Age = 26;
+let CurrentYear = 2022;
+console.log(CurrentYear - Age);
